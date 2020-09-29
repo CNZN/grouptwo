ad5208f99a7df6ae71e11208c49a6c9755c58531
OVERflow.hidden <798287855@qq.com>
2020-09-29 10:31:54 +0800

    zhus

--- src/route/index.js
+++ src/route/index.js
@@ -9,120 +9,149 @@ const router = new VueRouter({
         }, {
             path: '/find',
             name: 'Find',
-            component: () => import('../view/find/Find'),
+            component: () =>
+                import ('../view/find/Find'),
             redirect: "/find/gexing",
             children: [{
                 path: "gexing",
-                redirect:"gexing/tuijian",
-                component: () => import("../view/find/gexing/Gexing"),
+                redirect: "gexing/tuijian",
+                component: () =>
+                    import ("../view/find/gexing/Gexing"),
                 children: [{
                         path: "fm",
-                        component: () => import("../view/find/gexing/children/Fm"),
-                    }, 
+                        component: () =>
+                            import ("../view/find/gexing/children/Fm"),
+                    },
                     {
                         path: "tuijian",
-                        component: () => import("../view/find/gexing/children/Tuijian"),
+                        component: () =>
+                            import ("../view/find/gexing/children/Tuijian"),
                     }
                 ]
             }, {
                 path: "zhubo",
-                redirect:"zhubo/fufei",
-                component: () => import("../view/find/zhubo/Zhubo"),
+                redirect: "zhubo/fufei",
+                component: () =>
+                    import ("../view/find/zhubo/Zhubo"),
                 children: [{
-                    path: "paihang",
-                    component: () => import("../view/find/zhubo/children/Paihang"),
-                }, 
-                {
-                    path: "vip",
-                    component: () => import("../view/find/zhubo/children/Vip"),
-                },{
-                    path: "fufei",
-                    component: () => import("../view/find/zhubo/children/Fufei"),
-                }
-            ]
+                        path: "paihang",
+                        component: () =>
+                            import ("../view/find/zhubo/children/Paihang"),
+                    },
+                    {
+                        path: "vip",
+                        component: () =>
+                            import ("../view/find/zhubo/children/Vip"),
+                    }, {
+                        path: "fufei",
+                        component: () =>
+                            import ("../view/find/zhubo/children/Fufei"),
+                    }
+                ]
             }]
         },
         {
             path: '/video',
             name: 'Video',
-            component: () => import('../view/video/Video')
+            component: () =>
+                import ('../view/video/Video')
         },
         {
             path: '/mine',
             name: 'Mine',
-            component: () => import('../view/mine/Mine')
+            component: () =>
+                import ('../view/mine/Mine'),
+            children: [{
+                path: '/mine/music',
+                name: 'music',
+                component: () =>
+                    import ('../view/mine/music/index.vue')
+            }]
         },
         {
             path: '/friend',
             name: 'Friend',
-            component: () => import('../view/friend/Friend')
+            component: () =>
+                import ('../view/friend/Friend')
         },
         {
             path: '/account',
             name: 'Account',
-            component: () => import('../view/account/Account')
-        },{
-            path:'/video',
-            name:'Video',
-            component: () => import('../view/video/Video'),
+            component: () =>
+                import ('../view/account/Account')
+        }, {
+            path: '/video',
+            name: 'Video',
+            component: () =>
+                import ('../view/video/Video'),
             redirect: '/video/recommend',
-            children: [
-                {
+            children: [{
                     path: 'recommend',
-                    name:'Recommend',
-                    component:()=>import('../view/video/recommend/index')
+                    name: 'Recommend',
+                    component: () =>
+                        import ('../view/video/recommend/index')
                 },
                 {
                     path: 'music',
-                    name:' Music',
-                    component: () => import('../view/video/music/index'),
+                    name: ' Music',
+                    component: () =>
+                        import ('../view/video/music/index'),
                 },
                 {
                     path: 'mv',
-                    name:'Mv',
-                    component:()=>import('../view/video/mv/index')
+                    name: 'Mv',
+                    component: () =>
+                        import ('../view/video/mv/index')
                 },
                 {
                     path: 'showtime',
-                    name:'Showtime',
-                    component:()=>import('../view/video/showtime/index')
+                    name: 'Showtime',
+                    component: () =>
+                        import ('../view/video/showtime/index')
                 },
                 {
                     path: 'erciyuan',
-                    name:'Erciyuan',
-                    component:()=>import('../view/video/erciyuan/index')
+                    name: 'Erciyuan',
+                    component: () =>
+                        import ('../view/video/erciyuan/index')
                 },
             ]
-            
+
         },
         {
             path: '/play',
             name: 'Play',
-            component: () => import('../components/Play')
+            component: () =>
+                import ('../components/Play')
         },
         {
             path: '/songlist',
             name: 'Songlist',
-            component: () => import('../view/songlist/Songlist')
+            component: () =>
+                import ('../view/songlist/Songlist')
         },
         {
             path: '/top',
             name: 'Top',
-            component: () => import('../view/top/Top')
+            component: () =>
+                import ('../view/top/Top')
         },
         {
             path: '/diantaifenlei',
             name: 'Diantaifenlei',
-            component: () => import('../view/diantaifenlei/Diantaifenlei')
-        },{
-            path:'/play',
-            name:'Play',
-            component:()=>import('../components/Play')
+            component: () =>
+                import ('../view/diantaifenlei/Diantaifenlei')
+        }, {
+            path: '/play',
+            name: 'Play',
+            component: () =>
+                import ('../components/Play')
         },
         {
             path: '/mvplay',
-            name:'Mvplay',
-            component: () => import('../view/mvplay/mvplay'),
+            name: 'Mvplay',
+            component: () =>
+                import ('../view/mvplay/mvplay'),
         }
     ],
     linkActiveClass: 'active'
