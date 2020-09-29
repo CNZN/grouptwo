0b3e96a4b4e88ad1d6828a0838da2d8d86eb506e
wdy <1679179810@qq.com>
2020-09-29 08:20:56 +0800

    0929

---
+++ src/view/video/erciyuan/index.vue
@@ -0,0 +1,123 @@
+<!-- 组件说明 -->
+<template>
+  <div class="erciyuan">
+    <div class="erciyuanCon">
+      <div class="mvMore" @click="broadcast">
+        <img src="../../../assets/images/pianaiMV.jpg" alt="" />
+        <div class="mvup">
+          <p class="topRight">mv</p>
+          <p class="center"><van-icon name="play-circle-o" /></p>
+          <p class="bottomRight"><van-icon name="service-o" />330万</p>
+        </div>
+        <p class="mvDsc">
+          仙剑奇侠传原生大碟
+        </p>
+      </div>
+    </div>
+     <div class="erciyuanCon">
+      <div class="mvMore" @click="broadcast">
+        <img src="../../../assets/images/pianaiMV.jpg" alt="" />
+        <div class="mvup">
+          <p class="topRight">mv</p>
+          <p class="center"><van-icon name="play-circle-o" /></p>
+          <p class="bottomRight"><van-icon name="service-o" />330万</p>
+        </div>
+        <p class="mvDsc">
+          仙剑奇侠传原生大碟
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  data() {
+    return {};
+  },
+  computed: {},
+  watch: {},
+  methods: {
+      broadcast(){
+          this.$router.push({
+       path:'/mvplay'
+     }).catch(err=>{
+       console.log(err)
+     })
+      }
+  },
+  created() {},
+  mounted() {},
+  beforeCreate() {}, //生命周期 - 创建之前
+  beforeMount() {}, //生命周期 - 挂载之前
+  beforeUpdate() {}, //生命周期 - 更新之前
+  updated() {}, //生命周期 - 更新之后
+  beforeDestroy() {}, //生命周期 - 销毁之前
+  destroyed() {}, //生命周期 - 销毁完成
+  activated() {}, //如果页面有keep-alive缓存功能，这个函数会触发
+};
+</script>
+
+<style lang="scss" scoped>
+.erciyuan {
+  margin-top: 90px;
+}
+.erciyuanCon {
+  padding: 0 10px;
+}
+.mvMore {
+  width: 100%;
+  position: relative;
+  margin-bottom: 10px;
+  img {
+    width: 100%;
+    border-radius: 10px;
+    height: 200px;
+  }
+  border-bottom: 1px solid #efefef;
+  .mvup {
+    width: 100%;
+    height: 200px;
+    border-radius: 10px;
+    background: rgba(0, 0, 0, 0.2);
+    position: absolute;
+    top: 0;
+    .topRight {
+      width: 40px;
+      height: 20px;
+      color: #fff;
+      border: 1px solid #e4e2e2;
+      font-size: 18px;
+      line-height: 20px;
+      text-align: center;
+      border-radius: 10px;
+      background: rgba(134, 131, 131, 0.3);
+      position: absolute;
+      top: 10px;
+      right: 10px;
+    }
+    .bottomRight {
+      color: #fff;
+      position: absolute;
+      bottom: 10px;
+      right: 10px;
+      font-size: 12px;
+    }
+    .center {
+      position: absolute;
+      top: 70px;
+      left: 150px;
+      font-size: 50px;
+      color: #d8d8d4;
+    }
+  }
+  .mvDsc {
+    height: 40px;
+    line-height: 40px;
+    text-indent: 5px;
+    border-bottom: 1px solid #efefef;
+    color: #666;
+  }
+}
+</style>
---
+++ src/view/video/music/index.vue
@@ -0,0 +1,195 @@
+<!-- 组件说明 -->
+<template>
+  <div class="music">
+    <div class="swipeBox">
+      <van-swipe class="my-swipe" :autoplay="3000" indicator-color="white">
+        <van-swipe-item
+          ><img src="../../../assets/images/item0.jpg"
+        /></van-swipe-item>
+        <van-swipe-item
+          ><img src="../../../assets/images/item1.jpg"
+        /></van-swipe-item>
+        <van-swipe-item
+          ><img src="../../../assets/images/item2.jpg"
+        /></van-swipe-item>
+        <van-swipe-item
+          ><img src="../../../assets/images/item3.jpg"
+        /></van-swipe-item>
+        <van-swipe-item
+          ><img src="../../../assets/images/item4.jpg"
+        /></van-swipe-item>
+        <van-swipe-item
+          ><img src="../../../assets/images/item5.jpg"
+        /></van-swipe-item>
+        <van-swipe-item
+          ><img src="../../../assets/images/item6.jpg"
+        /></van-swipe-item>
+      </van-swipe>
+    </div>
+    <div class="newBox">
+      <div class="newTitle">
+        <h3>最新上线</h3>
+      </div>
+      <div class="newCon">
+        <div class="newConItem" @click="broadcast">
+          <img src="../../../assets/images/kexiMV.jpg" alt="" />
+          <p>超级面对面 第221期 等风来不如迎风跑</p>
+          <div class="itemBottom">
+            <span class="itemCount">
+              <van-icon name="service-o" />
+              <i>120万</i>
+            </span>
+            <span class="itemOn" @click="broadcast"
+              ><van-icon name="play-circle-o" class="icon"
+            /></span>
+          </div>
+        </div>
+        <div class="newConItem">
+          <img src="../../../assets/images/yuanzhouMV.jpg" alt="" />
+          <p>一周速报 EP20 快来体验吧</p>
+          <div class="itemBottom">
+            <span class="itemCount">
+              <van-icon name="service-o" />
+              <i>82万</i>
+            </span>
+            <span class="itemOn" @click="broadcast"
+              ><van-icon name="play-circle-o" class="icon"
+            /></span>
+          </div>
+        </div>
+      </div>
+      <div class="newCon">
+        <div class="newConItem">
+          <img src="../../../assets/images/kexiMV.jpg" alt="" />
+          <p>超级面对面 第221期 等风来不如迎风跑</p>
+          <div class="itemBottom">
+            <span class="itemCount">
+              <van-icon name="service-o" />
+              <i>120万</i>
+            </span>
+            <span class="itemOn" @click="broadcast"
+              ><van-icon name="play-circle-o" class="icon"
+            /></span>
+          </div>
+        </div>
+        <div class="newConItem">
+          <img src="../../../assets/images/yuanzhouMV.jpg" alt="" />
+          <p>一周速报 EP20 快来体验吧</p>
+          <div class="itemBottom">
+            <span class="itemCount">
+              <van-icon name="service-o" />
+              <i>82万</i>
+            </span>
+            <span class="itemOn" @click="broadcast"
+              ><van-icon name="play-circle-o" class="icon"
+            /></span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  data() {
+    return {};
+  },
+  computed: {},
+  watch: {},
+  methods: {
+    broadcast() {
+     this.$router.push({
+       path:'/mvplay'
+     }).catch(err=>{
+       console.log(err)
+     })
+    },
+  },
+  created() {},
+  mounted() {},
+  beforeCreate() {}, //生命周期 - 创建之前
+  beforeMount() {}, //生命周期 - 挂载之前
+  beforeUpdate() {}, //生命周期 - 更新之前
+  updated() {}, //生命周期 - 更新之后
+  beforeDestroy() {}, //生命周期 - 销毁之前
+  destroyed() {}, //生命周期 - 销毁完成
+  activated() {}, //如果页面有keep-alive缓存功能，这个函数会触发
+};
+</script>
+
+<style lang="scss" scoped>
+.music{
+  margin-top:80px;
+}
+.swipeBox {
+  padding: 20px 15px;
+}
+.my-swipe {
+  border-radius: 15px;
+}
+.my-swipe .van-swipe-item {
+  color: #fff;
+  font-size: 20px;
+  line-height: 150px;
+  text-align: center;
+  background-color: #39a9ed;
+  width: 100%;
+  height: 150px;
+  img {
+    height: 150px;
+  }
+}
+
+.newBox {
+  margin-top: 10px;
+  padding: 0 15px 20px;
+  .newTitle {
+    font-size: 18px;
+    font-weight: 600;
+  }
+}
+.newCon {
+  margin-top: 20px;
+  display: flex;
+  justify-content: space-around;
+  .newConItem {
+    width: 160px;
+    position: relative;
+    img {
+      width: 100%;
+      border-radius: 5px;
+    }
+    p {
+      margin-top: 10px;
+      font-size: 14px;
+      line-height: 18px;
+    }
+    .itemBottom{
+        position: absolute;
+        top:62px;
+        width: 160px;
+        height: 30px;
+        background: rgba(0,0,0,0.5);
+        display: flex;
+        color: #fff;
+        .itemCount{
+            font-size: 14px;
+            text-indent: 5px;
+            height: 30px;
+            line-height: 30px;
+        }
+        .itemOn{
+            margin-left: 60px;
+            height: 30px;
+            .icon{
+                font-size: 22px;
+                line-height: 30px;
+            }
+        }
+    }
+    
+  }
+}
+</style>
